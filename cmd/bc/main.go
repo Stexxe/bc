937c3df5aa0b7f69f99b6c409e7fe3e5c89ab14e
Aleksei.Tirman <aleksei.tirman@jetbrains.com>
2024-12-11 12:55:37 +0200

    Animate tank for all directions and fix movement when multiple buttons are pressed

--- cmd/bc/main.go
+++ cmd/bc/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	_ "embed"
+	"fmt"
+	"github.com/Stexxe/bc/internal/app/anim"
 	"github.com/Stexxe/bc/internal/app/util"
 	"github.com/veandco/go-sdl2/img"
 	"github.com/veandco/go-sdl2/sdl"
@@ -17,13 +19,23 @@ var tankPos = util.NewVector(100, 100)
 
 const tankSpeed = 0.1
 
+var tankAnim = "up"
+var tankAnims = map[string]anim.Descriptor{
+	"up":    {X: 1, Y: 130, FramesCount: 2},
+	"left":  {X: 34, Y: 129, FramesCount: 2},
+	"down":  {X: 65, Y: 129, FramesCount: 2},
+	"right": {X: 97, Y: 129, FramesCount: 2},
+}
+
+var pressedKey sdl.Keycode = sdl.K_UNKNOWN
+
 func main() {
 	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
 		log.Fatal(err)
 	}
 	defer sdl.Quit()
 
-	window, err := sdl.CreateWindow("PACMAN", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, 800, 600, sdl.WINDOW_SHOWN)
+	window, err := sdl.CreateWindow("BATTLE CITY", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, 800, 600, sdl.WINDOW_SHOWN)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -59,10 +71,7 @@ func main() {
 	}
 	defer texture.Destroy()
 
-	ssTankX := int32(1)
-	ssTankY := int32(130)
 	size := int32(13)
-	frames := int32(2)
 	curFrame := int32(0)
 	scale := int32(2)
 	gap := int32(3)
@@ -80,20 +89,27 @@ func main() {
 		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
 			switch e := event.(type) {
 			case *sdl.KeyboardEvent:
-				if e.Type == sdl.KEYDOWN {
+				if e.Type == sdl.KEYDOWN && pressedKey == sdl.K_UNKNOWN {
 					speed = tankSpeed
+					pressedKey = e.Keysym.Sym
 
-					switch e.Keysym.Sym {
+					switch pressedKey {
 					case sdl.K_w:
 						direction = util.VectorUp
+						tankAnim = "up"
 					case sdl.K_a:
 						direction = util.VectorLeft
+						tankAnim = "left"
 					case sdl.K_s:
 						direction = util.VectorDown
+						tankAnim = "down"
 					case sdl.K_d:
 						direction = util.VectorRight
+						tankAnim = "right"
 					}
-				} else if e.Type == sdl.KEYUP {
+				} else if e.Type == sdl.KEYUP && pressedKey == e.Keysym.Sym {
+					fmt.Println(sdl.GetKeyName(sdl.Keycode(pressedKey)), sdl.GetKeyName(sdl.Keycode(e.Keysym.Sym)))
+					pressedKey = sdl.K_UNKNOWN
 					speed = 0
 				}
 			case *sdl.QuitEvent:
@@ -109,18 +125,22 @@ func main() {
 			curFrame++
 		}
 
-		if curFrame >= frames {
+		ad, _ := tankAnims[tankAnim]
+		if curFrame >= ad.FramesCount {
 			curFrame = 0
 		}
 
 		tankPos = tankPos.Sum(direction.MulScalar(int32(speed * float32(delta))))
 
 		// Draw
-		renderer.SetDrawColor(169, 169, 169, 255)
+		renderer.SetDrawColor(0, 0, 1, 255)
 		renderer.Clear()
 
-		x := ssTankX + curFrame*(size+gap)
-		y := ssTankY
+		x := ad.X
+		y := ad.Y
+		if speed > 0 {
+			x += curFrame * (size + gap)
+		}
 
 		renderer.Copy(texture, &sdl.Rect{x, y, size, size}, &sdl.Rect{tankPos.X, tankPos.Y, size * scale, size * scale})
 		renderer.Present()
---
+++ internal/app/anim/anim.go
@@ -0,0 +1,6 @@
+package anim
+
+type Descriptor struct {
+	X, Y        int32
+	FramesCount int32
+}
